0275a054326e67892d9571ee4419505f820831a7
jsrj <arjay.jones3301@gmail.com>
2017-07-18 11:22:49 -0400

    Initial set up of gulp and browsersync for testing on mobile devices

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create(); // create a browser sync instance.
+
+gulp.task('browser-sync', function() {
+    bs.init({
+          proxy: "localhost:8080"
+    });
+});
