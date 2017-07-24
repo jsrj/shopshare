60e81ab248c32ff83f976cadffae823d108fae04
jsrj <arjay.jones3301@gmail.com>
2017-07-24 13:00:28 -0400

    added jquery and semantic ui functionality to app component

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
-var gulp = require('gulp');
-var bs = require('browser-sync').create(); // create a browser sync instance.
+const gulp = require('gulp');
+const bs   = require('browser-sync').create(); // create a browser sync instance.
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', () => {
     bs.init({
+          // this should be the URL of your ng server
+          // e.g. if you ran 'ng server -p 8080'
+          // ** More Settings can be configured here too, but for simply working on a mobile-first design, this should suffice **
           proxy: "localhost:8080"
     });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,103 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ShopShare';
 
-  clickTest() {
-    alert('thing has been clicked');
+  openMenu() {
+    $(document).ready(() => {
+            $('.left.sidebar')
+              .not('.styled')
+              .sidebar('setting', {
+                dimPage          : true,
+                transition       : 'overlay',
+                mobileTransition : 'overlay',
+                scrollLock: true,
+                returnScroll: true
+              })
+            ;
+            $('.left.sidebar').not('.styled').sidebar('toggle');
+    });
   }
+
+  navigateHome() {
+    alert('Home clicked');
+  }
+
+  navigateSrvc() {
+    alert('Services clicked');
+  }
+
+  navigateFrnd() {
+    alert('Friends clicked');
+  }
+
+  navigateHist() {
+    alert('History clicked');
+  }
+
+  navigateMsgs() {
+    alert('Messages clicked');
+  }
+
+  navigateCert() {
+    alert('Certifications clicked');
+  }
+
+  navigateSetn() {
+    alert('Settings clicked');
+  }
+
+// SORT-BY SECTION
+  sortByFacility() {
+    $('#sub-menu').children('.item').removeClass('active');
+    $('#facility').addClass('active');
+  }
+  sortByEquipmnt() {
+    $('#sub-menu').children('.item').removeClass('active');
+    $('#equipmnt').addClass('active');
+  }
+  sortByServices() {
+    $('#sub-menu').children('.item').removeClass('active');
+    $('#services').addClass('active');
+  }
+// SORT-BY SECTION
+    login() {
+      $('.ui#login-form')
+      .modal('show');
+    }
+
+
+// Registration Forms Sections
+    register() {
+      $('.ui.large.modal#register-form')
+      .modal('show');
+    }
+    hideWindow() {
+      $('.ui.modal').modal('hide');
+      $('.ui.modal').modal('hideDimmer');
+    }
+    continueToUserLocation() {
+      $('.ui.modal#location-form').modal('show');
+      $('.ui.modal#register-form').modal('hide');
+    }
+    backToCredentials() {
+      $('.ui.modal#register-form').modal('show');
+      $('.ui.modal#location-form').modal('hide');
+    }
+    submitRegistration() {
+
+    }
+// Registration Forms Section
+
 }
+// Displays jQuery version, and as such, confirms it is working
+console.log(`jQuery version: ${$.fn.jquery}`);
+
+// Non-(click) UI Events
+  $(document).ready(function() {
+    $('.ui.dropdown').dropdown()
+    ;
+    $('.ui.menu .dropdown').dropdown({on: 'hover'});
+  });
+
+
+
+
